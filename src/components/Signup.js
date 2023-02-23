5025fe8a13f53f06f03c58063e0d27b519f19cd3
Nica Weisinger <nica.weisinger@gmail.com>
2023-02-22 20:08:28 -0500

    Sign up page formatted with bootstrap

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,5 +1,5 @@
 import React, { useRef } from "react";
-import { Form } from "react-bootstrap";
+import { Card, Form, Button } from "react-bootstrap";
 
 export default function Signup() {
   const emailRef = useRef();
@@ -7,16 +7,36 @@ export default function Signup() {
   const passwordConfirmRef = useRef();
   return (
     <>
-      <form>
-        <h2 className="text-center mb-4">Sign Up</h2>
-        <label>Email</label>
-        <input type="email" ref={emailRef} required />
-        <label>Password</label>
-        <input type="password" ref={passwordRef} required />
-        <label>Password Confirmation</label>
-        <input type="password" ref={passwordConfirmRef} required />
-      </form>
-
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Sign Up</h2>
+          <Form>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" ref={emailRef} required></Form.Control>
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                ref={passwordRef}
+                required
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group id="password-confirm">
+              <Form.Label>Password Confirmtion</Form.Label>
+              <Form.Control
+                type="password"
+                ref={passwordConfirmRef}
+                required
+              ></Form.Control>
+            </Form.Group>
+            <Button className="w-100" type="submit">
+              Sign Up
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
       <div className="w-100 text-center mt-2">
         Already have an account? Log In
       </div>
